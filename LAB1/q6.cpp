9d20138c27ae67e07cd37e2343ee7fc5b224817e
Ali2006-NED <aliahmed23022006@gmail.com>
2025-09-30 21:07:03 +0500

    Corrected a logical bug

--- LAB1/q6.cpp
+++ LAB1/q6.cpp
@@ -1,40 +1,34 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int FindNumberFrequency(int arr[],int size)
-{
-    if (size == 0)
-    {
+void FindNumberFrequency(int arr[], int size) {
+    if (size == 0) {
         cout << "Array is empty.";
-        return 0;
+        return;
     }
-    int freq = 0;
-    int i = 0;
 
-    while(i < size)
-    {
+    bool visited[100] = {false};
+
+    for (int i = 0; i < size; i++) {
+        if (visited[i]) continue;
+
         int count = 1;
-        for(int j = i + 1; j < size; j++)
-        {
-            if(arr[i] == arr[j])
-            {
+        for (int j = i + 1; j < size; j++) {
+            if (arr[i] == arr[j]) {
+                visited[j] = true;
                 count++;
             }
         }
         cout << "Number " << arr[i] << " occurs " << count << " times." << endl;
-        freq++;
-        i += count;
     }
 }
 
-int main(){
-
+int main() {
     int arr[8];
     cout << "Enter 8 integers: ";
-    for(int i = 0; i < 8; i++) {
+    for (int i = 0; i < 8; i++) {
         cin >> arr[i];
     }
     FindNumberFrequency(arr, 8);
-    
     return 0;
 }
