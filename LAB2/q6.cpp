677effc73757c18f8b5233e96687d7bcf02ff692
Ali2006-NED <aliahmed23022006@gmail.com>
2025-10-01 22:07:39 +0500

    Modified file

--- LAB2/q6.cpp
+++ LAB2/q6.cpp
@@ -18,24 +18,25 @@ void printList(Node* head){
     cout << "NULL" << endl;
 }
 
-Node* sortList(Node* head) {
+Node* sortList(Node* head) {    //Example 1->2->3->4->5
     if(!head || !head->next) return head;
-    Node*curr = head;
-    Node*sorted = NULL;
-    while(curr != NULL){
-        Node*nextnode = curr->next;
-        if(sorted == NULL || sorted->data >= curr->data){
-            curr->next = sorted;
-            sorted = curr;
+    Node*curr = head;  //curr = 1
+    Node*sorted = NULL; //sorted = NULL
+    while(curr != NULL){ //curr = 1 curr = 2 curr = 3
+        Node*nextnode = curr->next; //nextnode = 2 nextnode = 3 nextnode = 4
+        if(sorted == NULL || sorted->data >= curr->data){ //(T||F) (F||(1>=2)F) (F||(2>=3)F)
+            curr->next = sorted; //1->next = NULL
+            sorted = curr; //sorted = 1
         } else {
-            Node*temp = sorted;
-            while(temp->next != NULL && temp->next->data < curr->data){
-                temp = temp->next;
+            Node*temp = sorted; //temp = 1 temp = 1
+            while(temp->next != NULL && temp->next->data < curr->data){ //(T && (2<3)T)
+                temp = temp->next; 
             }
-            curr->next = temp->next;
-            temp->next = curr;
+            // temp=2
+            curr->next = temp->next; //2->next = NULL  3->next = NULL
+            temp->next = curr; //1->next = 2 2->next = 3
         }
-        curr = nextnode;
+        curr = nextnode; //curr = 2 curr = 3
 
     }
     return sorted;
